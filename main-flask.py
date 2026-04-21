ea05d5e33201693e81e04beaea1c5ae327a6b752
boaznumberd5555 <117567038+boaznumberd5555@users.noreply.github.com>
2026-04-21 11:11:10 +0800

    basic boaz commit

--- main-flask.py
+++ main-flask.py
@@ -1,1 +1,10 @@
+from flask import *
+import sqlite3
+app = Flask(__name__)
+@app.route("/", methods["GET","PULL"])
+def mainPage():
+  renderTemplate("MainPage.html")
 
+
+if "__name__" == "__main__":
+  app.run()
