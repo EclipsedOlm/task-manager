05de4631c5b28db8956ed5e266185e154fbf2157
ChessMaster1303 <57945166+ChessMaster1303@users.noreply.github.com>
2026-04-22 21:33:08 +0800

    supabase conn
    fixed connection to supabase db. can run sql queries now

--- database-test.py
+++ database-test.py
@@ -1,8 +1,16 @@
 import psycopg2
 
-PASSKEY = "justiceforhina10"
-link = f"postgresql://postgres:{PASSKEY}@db.lwolbogymjkydwumhepb.supabase.co:5432/postgres"
-conn = psycopg2.connect(link)
+# Put it into some secret variable later on
+PASSKEY = "justiceforhina10" # POOR HINA I KNOW WHAT YOU FEEL
 
-cursor = conn.execute("SELECT * FROM test")
-print(cursor)
+# PLEASE PLEASE PLEASE DON'T TOUCH THIS IT WORKS!!!
+# Apparently connecting with IPv6 (which is what we all do apparently) only works with this "pooling"
+# And DOES NOT work with Direct Connection (which uses IPv4 I think - we didn't pay for this)
+link = "postgresql://postgres.lwolbogymjkydwumhepb:{}@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres".format(PASSKEY) # Keeping passkeys secret!
+conn = psycopg2.connect(link) # Connect to the supabase db
+cursor = conn.cursor() # The cursor object is the one that executes the SQL code
+
+# testing sql query
+# note that psycopg2 has slightly diff syntax
+cursor.execute("SELECT * FROM test")
+print(cursor.fetchall())
