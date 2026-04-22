cc34a3c3cbca11f16c40f9b2948bf39688f72512
ChessMaster1303 <57945166+ChessMaster1303@users.noreply.github.com>
2026-04-22 21:55:35 +0800

    file cleanup
    i swear im not farming commits im just cleaning up files and renaming files to typical nomenclature

---
+++ main.py
@@ -0,0 +1,16 @@
+# Imports: 
+from flask import Flask, render_template, request # Flask functionality
+import psycopg2 # Database functionality
+
+app = Flask(__name__) # Create the Flask app
+
+
+# Main homepage
+@app.route("/", methods=["GET","POST"])
+def index():
+  return render_template("index.html") # index.html is default naming convention for the landing page
+
+
+# Run the app
+if __name__ == "__main__":
+  app.run(port=5000)
