09651ae5ab4194006b510d3d9d7f3fb55f7c87a9
ChessMaster1303 <57945166+ChessMaster1303@users.noreply.github.com>
2026-05-06 21:28:01 +0800

    secret passkey

--- db_funcs.py
+++ db_funcs.py
@@ -1,13 +1,11 @@
 import psycopg2 # Database access library
 from psycopg2.extras import RealDictCursor # Essentially row_factory of sqlite3
-
-# Put it into some secret variable later on
-PASSKEY = "justiceforhina10" # POOR HINA I KNOW WHAT YOU FEEL
+import os # To get environment variables
 
 # PLEASE PLEASE PLEASE DON'T TOUCH THIS IT WORKS!!!
 # Apparently connecting with IPv6 (which is what we all do apparently) only works with this "pooling"
 # And DOES NOT work with Direct Connection (which uses IPv4 I think - we didn't pay for this)
-link = "postgresql://postgres.lwolbogymjkydwumhepb:{}@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres".format(PASSKEY) # Keeping passkeys secret!
+link = "postgresql://postgres.lwolbogymjkydwumhepb:{}@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres".format(os.environ["PASSKEY"]) # Keeping passkeys secret!
 conn = psycopg2.connect(link) # Connect to the Supabase database
 cursor = conn.cursor(cursor_factory=RealDictCursor) # The cursor object is the one that executes the SQL code
 
